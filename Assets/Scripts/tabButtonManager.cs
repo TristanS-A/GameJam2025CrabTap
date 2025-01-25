a08b947f71d99931b1f3427ae2110fc900a7e9f9
Tristan Schonfeldt-Aultman <tSchonfeldt-Aultman@mtl.local>
2025-01-25 13:08:17 -0500

    Started tab manager for adding and resizing tabs.

--- Assets/Scripts/tabButtonManager.cs
+++ Assets/Scripts/tabButtonManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class tabButtonManager : MonoBehaviour
 {
+    [SerializeField] private GameObject tabPrefab;
     public List<Button> purchasedDomains;
     [SerializeField] Button websiteTabTemplate;
     [SerializeField] Canvas canvas;
+    private RectTransform tabBarRect; 
+
+    private List<GameObject> tabButtons = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tabBarRect = GetComponent<RectTransform>();
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("Hello");
+        addTab("End");
     }
 
     // Update is called once per frame
@@ -27,10 +48,38 @@ public class tabButtonManager : MonoBehaviour
     }
 
     //Function to add a new button
-    public void addButton()
+    public void addTab(string title)
     {
-        Button b = Instantiate(websiteTabTemplate);
-        b.transform.SetParent(canvas.transform);
-        //b.onClick.AddListener(toWebsite());
+        GameObject newTab = Instantiate(tabPrefab);
+        newTab.GetComponentInChildren<TextMeshProUGUI>().text = title;
+        newTab.transform.SetParent(transform);
+        RectTransform newTabRect = newTab.GetComponent<RectTransform>();
+        Vector2 newTabPos = new Vector2(-(int)(tabBarRect.sizeDelta.x / 2), 0);
+
+        float totalTabLength = 0;
+        foreach (GameObject tabOBJ in tabButtons)
+        {
+            RectTransform tabRect = tabOBJ.GetComponent<RectTransform>();
+            totalTabLength += tabRect.sizeDelta.x * tabRect.localScale.x;
+        }
+
+        if (totalTabLength > tabBarRect.sizeDelta.x)
+        {
+            float newWidth = tabBarRect.sizeDelta.x / totalTabLength;
+
+            foreach (GameObject tabOBJ in tabButtons)
+            {
+                RectTransform tabRect = tabOBJ.GetComponent<RectTransform>();
+                tabRect.sizeDelta = new Vector2(tabRect.sizeDelta.x * newWidth, tabRect.sizeDelta.y);
+
+                ////RESIZE BUTTONS AS WELL
+            }
+        }
+
+        ////REPOSITION THE BUTTONS
+
+        newTabPos += new Vector2(totalTabLength, 0);
+        newTabRect.localPosition = newTabPos;
+        tabButtons.Add(newTab);
     }
 }
