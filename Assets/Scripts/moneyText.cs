9e5662018a7d1fee34854211fcf97cf01580bc27
AlexanderDaley <The42ndNapkin@gmail.com>
2025-01-25 12:14:57 -0500

    Finished bank window prototype

--- Assets/Scripts/moneyText.cs
+++ Assets/Scripts/moneyText.cs
@@ -7,10 +7,15 @@ public class moneyText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI t;
     [SerializeField] private beanKounter playerMoney;
+    void Start()
+    {
+        playerMoney = GameObject.FindWithTag("money").GetComponent<beanKounter>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        t.text = (Mathf.Round(playerMoney.getMonee()* 100f) / 100f).ToString();
+        playerMoney.addMonee(0.05f);
+        t.text = "$" + (Mathf.Round(playerMoney.getMonee()* 100f) / 100f).ToString();
     }
 }
